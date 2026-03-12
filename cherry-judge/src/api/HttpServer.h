de49411451b54b8b1f47bcc5a42865522ddbcb7f
charon2121 <2833121040@qq.com>
2026-03-12 10:26:27 +0800

    update

---
+++ cherry-judge/src/api/HttpServer.h
@@ -0,0 +1,14 @@
+#ifndef CHERRY_API_HTTP_SERVER_H
+#define CHERRY_API_HTTP_SERVER_H
+
+namespace cherry{
+namespace api {
+
+class HttpServer {
+
+};
+
+} // namespace api
+} // namespace cherry 
+
+#endif
